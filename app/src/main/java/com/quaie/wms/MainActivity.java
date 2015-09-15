d11e0d2753f94e5b904935452149890bf0746ffa
lifuyuan <lifuyuan33@gmail.com>
2015-09-15 10:20:30 +0800

    README

---
+++ app/src/main/java/com/quaie/wms/MainActivity.java
@@ -0,0 +1,40 @@
+package com.quaie.wms;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.zxing.activity.CaptureActivity;
+
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private Button btnScan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnScan = (Button)findViewById(R.id.btnScan);
+        btnScan.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, CaptureActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            String result = data.getExtras().getString("result");
+            Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
