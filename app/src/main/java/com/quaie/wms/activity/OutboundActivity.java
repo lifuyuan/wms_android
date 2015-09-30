236ff420e54ae0dc85315d250ab86a6a60dc9cb7
lifuyuan <lifuyuan33@gmail.com>
2015-09-30 13:13:29 +0800

    n

--- app/src/main/java/com/quaie/wms/activity/OutboundActivity.java
+++ app/src/main/java/com/quaie/wms/activity/OutboundActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +21,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.quaie.wms.Config;
 import com.quaie.wms.R;
+import com.quaie.wms.utils.Logger;
 import com.quaie.wms.utils.TitleBuilder;
 import com.quaie.wms.utils.ToastUtils;
 import com.utils.SerializableMap;
@@ -37,118 +41,95 @@ import java.util.Map;
 public class OutboundActivity   extends Activity {
 
     protected String TAG;
-    private TextView barcodeResultTextView;
-    private TextView orderResultTextView;
-    private Button btnScanOrder;
-    private Button btnScanCommodity;
-    private Button btnSubmit;
-    private HashMap<String, Object> result;
-    private int commodity_amount = 0;
-    private JSONArray result_array_barcode;
-    private String result_array_order;
+    private EditText etBarcode;
+    private EditText etOrder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_outbound);
         new TitleBuilder(OutboundActivity.this).setTitleText("Order Pack");
-        orderResultTextView = (TextView)this.findViewById(R.id.tv_scan_order);
-        barcodeResultTextView = (TextView)this.findViewById(R.id.tv_scan_commodity1);
-
-        btnScanOrder = (Button)findViewById(R.id.btnScanOrder);
-        btnScanOrder.setOnClickListener(new View.OnClickListener() {
+        etBarcode = (EditText)findViewById(R.id.et_order_barcode);
+        etOrder = (EditText)findViewById(R.id.et_order);
+        etOrder.setOnKeyListener(new View.OnKeyListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OutboundActivity.this, CaptureActivity.class);
-                intent.putExtra("isContinue", "no");
-                intent.putExtra("scanType", "order_no");
-                startActivityForResult(intent, 0);
-            }
-        });
-
-        btnScanCommodity = (Button)findViewById(R.id.btnScanCommodity1);
-        btnScanCommodity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(OutboundActivity.this, CaptureActivity.class);
-                intent.putExtra("isContinue", "yes");
-                intent.putExtra("scanType", "outbound_commodity");
-                startActivityForResult(intent, 0);
-            }
-        });
-
-        btnSubmit = (Button)findViewById(R.id.btnSubmit3);
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final ProgressDialog pd = ProgressDialog.show(OutboundActivity.this, "Connecting", "Connecting to server,please wait");
-                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
-                String url = "http://www.24upost.com:5001/wms/android/unbound";
-                StringRequest request = new StringRequest(Request.Method.POST, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String result) {
-                                pd.dismiss();
-                                ToastUtils.showToast(OutboundActivity.this, "Successful", Toast.LENGTH_SHORT);
-                                OutboundActivity.this.finish();
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                final HashMap<String, Object> commodities = new HashMap();
+                final String orderNo = etOrder.getText().toString();
+                final JSONArray result_array_barcode;
+                String barcodes = etBarcode.getText().toString();
+                String[] abarcodes = barcodes.split("\n");
+                if(!TextUtils.isEmpty(orderNo)) {
+                    if (orderNo.length() > 1) {
+                        Logger.show(TAG,"111111111111111111111");
+                        for (int i = 0; i < abarcodes.length; i++) {
+                            int barcode_num = 1;
+                            Iterator iter = commodities.entrySet().iterator();
+                            while (iter.hasNext()) {
+                                Map.Entry entry = (Map.Entry) iter.next();
+                                String key = (String) entry.getKey();
+                                Integer value = (Integer) entry.getValue();
+                                if (abarcodes[i].equals(key)) {
+                                    barcode_num = value + 1;
+                                }
                             }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        pd.dismiss();
-                        ToastUtils.showToast(OutboundActivity.this, "Error", Toast.LENGTH_SHORT);
+                            commodities.put(abarcodes[i], barcode_num);
+                        }
+                        result_array_barcode = new JSONArray();
+                        Iterator iter = commodities.entrySet().iterator();
+                        while (iter.hasNext()) {
+                            Map.Entry entry = (Map.Entry) iter.next();
+                            String key = (String) entry.getKey();
+                            Integer value = (Integer) entry.getValue();
+
+                            JSONObject jsonmap1 = new JSONObject();
+                            try {
+                                jsonmap1.put("commodityBarcode", key);
+                                jsonmap1.put("quantity", value);
+                                result_array_barcode.put(jsonmap1);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                        final ProgressDialog pd = ProgressDialog.show(OutboundActivity.this, "Connecting", "Connecting to server,please wait");
+                        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                        String url = "http://www.24upost.com:5001/wms/android/unbound";
+                        StringRequest request = new StringRequest(Request.Method.POST, url,
+                                new Response.Listener<String>() {
+                                    @Override
+                                    public void onResponse(String result) {
+                                        pd.dismiss();
+                                        ToastUtils.showToast(OutboundActivity.this, "Successful", Toast.LENGTH_SHORT);
+
+                                    }
+                                }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                pd.dismiss();
+                                ToastUtils.showToast(OutboundActivity.this, "Error" + error.networkResponse.statusCode, Toast.LENGTH_SHORT);
+                            }
+                        }){
+                            @Override
+                            protected Map<String, String> getParams() throws AuthFailureError {
+                                HashMap<String, String> map = new HashMap<String, String>();
+                                map.put("token", Config.getCachedToken(OutboundActivity.this));
+                                map.put("orderNo", orderNo);
+                                map.put("outboundBarcode", result_array_barcode.toString());
+                                return map;
+                            }
+                        };
+                        request.setTag("wmsPost");
+                        queue.add(request);
+                        queue.start();
                     }
-                }){
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        HashMap<String, String> map = new HashMap<String, String>();
-                        map.put("token", Config.getCachedToken(OutboundActivity.this));
-                        map.put("orderNo", result_array_order);
-                        map.put("outboundBarcode", result_array_barcode.toString());
-                        return map;
-                    }
-                };
-                request.setTag("wmsPost");
-                queue.add(request);
-                queue.start();
+                    etBarcode.setText(null);
+                    etOrder.setText(null);
+                    etBarcode.setFocusable(true);
+                    etOrder.setFocusable(false);
+                }
+                return false;
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            SerializableMap resultmap = (SerializableMap)data.getExtras().getSerializable("result");
-            String scanType = data.getStringExtra("scanType");
-            result = (HashMap<String, Object>)resultmap.getMap();
-            Iterator iter = result.entrySet().iterator();
-            if (scanType.equals("outbound_commodity")) {
-                result_array_barcode = new JSONArray();
-                while (iter.hasNext()) {
-                    Map.Entry entry = (Map.Entry) iter.next();
-                    String key = (String) entry.getKey();
-                    Integer value = (Integer) entry.getValue();
-                    commodity_amount = commodity_amount + value;
-
-                    JSONObject jsonmap1 = new JSONObject();
-                    try {
-                        jsonmap1.put("commodityBarcode", key);
-                        jsonmap1.put("quantity", value);
-                        result_array_barcode.put(jsonmap1);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-                barcodeResultTextView.setText(result_array_barcode.toString());
-            }
-            else if (scanType.equals("order_no")) {
-                while (iter.hasNext()) {
-                    Map.Entry entry = (Map.Entry) iter.next();
-                    result_array_order = (String) entry.getKey();
-                }
-                orderResultTextView.setText(result_array_order);
-            }
-        }
-    }
 }
